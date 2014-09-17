b1ff142f3bef61c8d71653a72f482e3a6b05ff1d
David Björklund <david.bjorklund@gmail.com>
2014-09-17 19:39:45 +0200

    add background image

--- image-cropper.js
+++ image-cropper.js
@@ -5,45 +5,61 @@ var dom = require('dom-events')
   , resetZoom = require('./lib/reset-zoom')
   , resizeImage = require('./lib/resize-image')
 
-  , ImageCropper = function (containerElm, image, options) {
-      this._containerElm = containerElm;
-      this._imageElm = image;
-      this._width = options.width;
-      this._height = options.height;
-      this._enabled = false;
+  , ImageCropper = function (containerElm, croppedImage, backgroundImage, options) {
+      this._containerElm = containerElm
+      this._croppedImage = croppedImage
+      this._backgroundImage = backgroundImage
+      this._width = options.width
+      this._height = options.height
+      this._enabled = false
+
+      this._backgroundImage.src = options.src
 
       this._wrap()
       this._makeDraggable()
     }
 
   , init = function (containerElm, options, callback) {
-      var image = new Image()
-        , imageCropper = new ImageCropper(containerElm, image, options)
+      var croppedImage = new Image()
+        , backgroundImage = new Image()
+        , imageCropper = new ImageCropper(containerElm, croppedImage, backgroundImage, options)
 
-      image.src = options.src
+      croppedImage.src = options.src
+      backgroundImage.src = options.src
 
-      imageLoaded(image, function (err) {
+      imageLoaded(croppedImage, function (err) {
         if (err)
           return callback(err)
 
-        resetZoom(image, options.width, options.height)
+        resetZoom(croppedImage, options.width, options.height)
 
-        callback(null, imageCropper)
+        imageLoaded(backgroundImage, function (err) {
+          if (err)
+            return callback(err)
+
+          resetZoom(backgroundImage, options.width, options.height)
+
+          callback(null, imageCropper)
+
+        })
       })
     }
 
 ImageCropper.prototype.enable = function () {
-  this._enabled = true;
-  this._imageElm.style.cursor = 'move';
+  this._enabled = true
+  this._croppedImage.style.cursor = 'move'
+  this._backgroundImage.style.opacity = '0.5'
 }
 
 ImageCropper.prototype.disable = function () {
-  this._enabled = false;
-  this._imageElm.style.cursor = '';
+  this._enabled = false
+  this._croppedImage.style.cursor = ''
+  this._backgroundImage.style.opacity = '0'
 }
 
 ImageCropper.prototype._resizeImage = function (changeFactor) {
-  resizeImage(this._imageElm, changeFactor, this._width, this._height)
+  resizeImage(this._backgroundImage, changeFactor, this._width, this._height)
+  resizeImage(this._croppedImage, changeFactor, this._width, this._height)
 }
 
 ImageCropper.prototype.zoomIn = function () {
@@ -66,22 +82,37 @@ ImageCropper.prototype._wrap = function () {
   container.style.width = this._width
   container.style.height = this._height
   container.style.position = 'relative'
-  container.style.overflow = 'hidden'
 
-  this._imageElm.style.position = 'absolute'
+  this._backgroundImage.style.position = 'absolute'
+  this._backgroundImage.style.opacity = '0'
+  this._backgroundImage.style['z-index'] = '1000'
 
-  container.appendChild(this._imageElm)
+  var cropContainer = document.createElement('div')
+
+  cropContainer.style.width = this._width
+  cropContainer.style.height = this._height
+  cropContainer.style.position = 'absolute'
+  cropContainer.style.overflow = 'hidden'
+
+  this._croppedImage.style.position = 'absolute'
+
+  cropContainer.appendChild(this._croppedImage)
+
+  container.appendChild(this._backgroundImage)
+  container.appendChild(cropContainer)
 }
 
 ImageCropper.prototype._moveImage = function (leftChange, topChange) {
-  moveImage(this._imageElm, leftChange, topChange, this._width, this._height)
+  moveImage(this._backgroundImage, leftChange, topChange, this._width, this._height)
+  moveImage(this._croppedImage, leftChange, topChange, this._width, this._height)
 }
 
 ImageCropper.prototype._makeDraggable = function () {
   var self = this
 
   // subscribe for mousedown on the image element
-  dom.on(this._imageElm, 'mousedown', function () {
+  // todo: make some nicer abstraction of this
+  dom.on(this._backgroundImage, 'mousedown', function () {
     if (!self._enabled)
       return
 

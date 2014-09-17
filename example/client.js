2eb69eba6c03717ca3d724b331355c34b40497b2
David Björklund <david.bjorklund@gmail.com>
2014-09-17 12:09:57 +0200

    cleanup, set height to 80

--- example/client.js
+++ example/client.js
@@ -3,9 +3,9 @@ var dom = require('dom-events')
   , imageCropper = require('../image-cropper.js')
 
 dom.on(window, 'load', function() {
-  var image = image = document.querySelector('#image')
+  var image = document.querySelector('#image')
 
-  imageCropper(image, { width: 100, height: 100 }, function (err, cropper) {
+  imageCropper(image, { width: 100, height: 80 }, function (err, cropper) {
 
     dom.on(document.querySelector('#zoom-in'), 'click', function () {
       cropper.zoomIn()

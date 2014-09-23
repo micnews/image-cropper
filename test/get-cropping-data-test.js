52b182f8f6907e84bc02a96fba1708203782cd71
David Björklund <david.bjorklund@gmail.com>
2014-09-23 11:37:44 +0200

    getCroppingData: work correctly when zoomed in

--- test/get-cropping-data-test.js
+++ test/get-cropping-data-test.js
@@ -23,3 +23,22 @@ test('simple', function (t) {
 
   t.end()
 })
+
+test('zoomed in', function (t) {
+  var options = {
+          image: common.testImage(200, 200)
+        , container: document.createElement('div')
+      }
+
+  options.container.style.width = '200px'
+  options.container.style.height = '200px'
+  // image is zoomed in to double the size
+  options.image.width = 400
+  options.image.height = 400
+
+  options.image.style.left = '-100px'
+  options.image.style.top = '-200px'
+
+  t.deepEqual(getCroppingData(options), { left: 50, top: 100, right: 50, bottom: 0 })
+  t.end()
+})

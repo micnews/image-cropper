80f9e74a6ee3fa2878c5d02bbcdfd35d55b01aeb
David Björklund <david.bjorklund@gmail.com>
2014-10-01 11:56:18 +0200

    add enable/disable functionality to draggable

--- test/draggable-test.js
+++ test/draggable-test.js
@@ -1,17 +1,17 @@
 var dom = require('dom-events')
   , test = require('tape')
 
-  , draggable = require('../lib/draggable')
+  , makeDraggable = require('../lib/draggable')
 
 test('drag within element', function (t) {
   var elm = document.createElement('div')
 
   document.body.appendChild(elm)
 
-  draggable(elm, function (event) {
+  makeDraggable(elm, function (event) {
     t.equal(event.customProperty, 'oh yeah')
     t.end()
-  })
+  }).enable()
 
   dom.emit(elm, 'mousedown')
   dom.emit(elm, 'mousemove', { bubbles: true, customProperty: 'oh yeah' })
@@ -23,12 +23,50 @@ test('start drag within element drag outside', function (t) {
 
   document.body.appendChild(elm)
 
-  draggable(elm, function (event) {
+  makeDraggable(elm, function (event) {
     t.equal(event.customProperty, 'ohhhh yeah')
     t.end()
-  })
+  }).enable()
 
   dom.emit(elm, 'mousedown')
   dom.emit(document, 'mousemove', { customProperty: 'ohhhh yeah' })
   dom.emit(document, 'mouseup')
+})
+
+test('start disabled', function (t) {
+  var elm = document.createElement('div')
+
+  document.body.appendChild(elm)
+
+  makeDraggable(elm, function () {
+    t.fail('should not call callback')
+  })
+
+  dom.emit(elm, 'mousedown')
+  dom.emit(document, 'mousemove')
+  dom.emit(document, 'mouseup')
+
+  process.nextTick(function () {
+    t.end()
+  })
+})
+
+test('disable()', function (t) {
+  var elm = document.createElement('div')
+   , draggable = makeDraggable(elm, function (event) {
+        t.fail('should not call callback')
+      })
+
+  document.body.appendChild(elm)
+
+  draggable.enable()
+  draggable.disable()
+
+  dom.emit(elm, 'mousedown')
+  dom.emit(document, 'mousemove')
+  dom.emit(document, 'mouseup')
+
+  process.nextTick(function () {
+    t.end()
+  })
 })

d5083d280928328ceec187013200f6da4756fb9a
David Björklund <david.bjorklund@gmail.com>
2014-10-01 13:00:31 +0200

    let result-image also ensure it's in the container

--- test/result-image-test.js
+++ test/result-image-test.js
@@ -3,8 +3,9 @@ var test = require('tape')
   , common = require('./common')
 
 test('basic', function (t) {
-  var imageSrc = common.createTestImageSrc(100, 100)
-    , image = resultImage({ src: imageSrc })
+  var container = document.createElement('div')
+    , imageSrc = common.createTestImageSrc(100, 100)
+    , image = resultImage({ src: imageSrc, container: container })
 
   t.equal(image.src, imageSrc, 'should have correct image src')
   t.equal(image.style.position, 'absolute')
@@ -13,14 +14,38 @@ test('basic', function (t) {
   t.equal(image.style.opacity, '1')
   t.equal(image.width, 100)
   t.equal(image.height, 100)
+  t.ok(image.classList.contains('result-image'), 'should have correct class')
+  t.equal(container.querySelector('img.result-image'), image, 'image is added to container')
+
   t.end()
 })
 
-test('custom width', function (t) {
-  var imageSrc = common.createTestImageSrc(100, 100)
-    , image = resultImage({ src: imageSrc, width: 120, height: 120 })
+test('custom width & height', function (t) {
+  var container = document.createElement('div')
+    , imageSrc = common.createTestImageSrc(100, 100)
+    , image = resultImage({ src: imageSrc, width: 120, height: 120, container: container })
 
   t.equal(image.width, 120)
   t.equal(image.height, 120)
+  t.end()
+})
+
+test('reuse existing image element', function (t) {
+  var container = document.createElement('div')
+    , imageSrc = common.createTestImageSrc(100, 100)
+    , expectedImage = (function () {
+        var image = new Image()
+
+        image.classList.add('result-image')
+        image.classList.add('beep-boop')
+
+        container.appendChild(image)
+
+        return image
+      })()
+    , actualImage = resultImage({ src: imageSrc, container: container })
+
+  t.equal(actualImage.nodeName, 'IMG', 'should return image')
+  t.equal(actualImage, expectedImage, 'should reuse existing image')
   t.end()
 })
